6f08c7b2c2fa4ed46cfdd3225d070c227f26fa94
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2025-12-19 00:04:46 +0700

    init: v1 source structure

--- lib/pl_image_downloader.dart
+++ lib/pl_image_downloader.dart
@@ -1,8 +1,1 @@
 
-import 'pl_image_downloader_platform_interface.dart';
-
-class PlImageDownloader {
-  Future<String?> getPlatformVersion() {
-    return PlImageDownloaderPlatform.instance.getPlatformVersion();
-  }
-}
--- test/pl_image_downloader_method_channel_test.dart
+++ test/pl_image_downloader_method_channel_test.dart
@@ -1,27 +1,5 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pl_image_downloader/pl_image_downloader_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  MethodChannelPlImageDownloader platform = MethodChannelPlImageDownloader();
-  const MethodChannel channel = MethodChannel('pl_image_downloader');
-
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
-
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
 }
