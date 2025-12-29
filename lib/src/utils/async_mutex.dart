50d1d5222eb46262f93dda57216badd460f0c065
Nguyễn Minh Hưng <90996598+nguyenminhhung2011@users.noreply.github.com>
2025-12-29 14:37:42 +0700

    feat: impl async mutext for asynchronous operations

--- lib/src/utils/async_mutex.dart
+++ lib/src/utils/async_mutex.dart
@@ -1,1 +1,27 @@
-class AsyncMutex {}
+import 'dart:async';
+
+/// AsyncMutex
+/// This class is used to create a mutex for asynchronous operations.
+class AsyncMutex {
+  Future _running = Future.value();
+
+  /// Enqueued
+  int _enqueued = 0;
+
+  /// Enqueued
+  int get enqueued => _enqueued;
+
+  /// Run
+  /// This method is used to run a function asynchronously.
+  Future<T> run<T>(Future<T> Function() fn) async {
+    final completer = Completer<T>();
+
+    _enqueued++;
+    _running.whenComplete(() {
+      _enqueued--;
+      completer.complete(Future<T>.sync(fn));
+    });
+
+    return _running = completer.future;
+  }
+}
